02aff7ca7849211a2c07a35b711470b4dd6bc30e
C.A.Chen <acechen@cirx.org>
2015-07-08 14:24:42 +0800

    update oven shutter

--- oven_shutter_driver/oven_shutter_driver.ino
+++ oven_shutter_driver/oven_shutter_driver.ino
@@ -4,27 +4,23 @@ Servo myservo;
 
 void setup() 
 { 
-  attachInterrupt(3, OpenShutter, RISING);
-  attachInterrupt(3, CloseShutter, FALLING);
+  pinMode(13, OUTPUT);
+  pinMode(3 , INPUT);
   myservo.attach(9);
   delay(1000);
 } 
 
 void loop() 
 {
-  
+  if (digitalRead(3)){
+    myservo.write(70);
+    digitalWrite(13, HIGH);
+    delay(500);
+  }
+  if (!digitalRead(3)){  
+    myservo.write(0);
+    digitalWrite(13, LOW);
+    delay(500);
+  }
 }
 
-void OpenShutter()
-{
-  myservo.write(70);
-  digitalWrite(13, HIGH);
-  delay(500);
-}
-
-void CloseShutter()
-{
-  myservo.write(0);
-  digitalWrite(13, LOW);
-  delay(500);
-}
